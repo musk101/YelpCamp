ad6593cd3c4c1ef7dde40b96c99729c852d75245
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2020-10-27 11:40:58 +0530

    add

--- v1/app.js
+++ v1/app.js
@@ -7,10 +7,20 @@ res.render("landing");
 });
 app.get("/campgrounds",function(req,res)
 {
-res.render(" campground landing");
+var campgrounds =[
+    {name : "mi" , image: "https://i.ibb.co/9pSw9dy/download.jpg" }
+]
+res.render("campgrounds", {campgrounds:campgrounds});
+});
+app.post("/campgrounds" , function(req,res)
+{
+console.log("you hit post!")
 });
 
-app.listen(process.env.PORT,process.env.IP , function()
+
+
+
+app.listen(3000 , function()
 {
     console.log("Yelp Camp has started!");
 });
