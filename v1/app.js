f1c93e9a464bbe02592041c6fe4a666a5b543013
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2020-10-27 15:05:11 +0530

    add

--- v1/app.js
+++ v1/app.js
@@ -1,20 +1,29 @@
 var express = require("express");
 var app = express();
 app.set("view engine" , "ejs");
+var campgrounds =[
+    {name : "mi" , image: "https://i.ibb.co/9pSw9dy/download.jpg" }
+];
 app.get("/",function(req,res)
 {
 res.render("landing");
 });
 app.get("/campgrounds",function(req,res)
 {
-var campgrounds =[
-    {name : "mi" , image: "https://i.ibb.co/9pSw9dy/download.jpg" }
-]
 res.render("campgrounds", {campgrounds:campgrounds});
 });
 app.post("/campgrounds" , function(req,res)
 {
-console.log("you hit post!")
+    var name=req.body.name;
+    var image=req.body.image;
+    var newcampground = {name:name, image:image}
+    campgrounds.push(newcampground);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new",function(req,res)
+{
+    res.render("new.ejs");
 });
 
 
